591ec83502992b5e74dca5b13cd2053815f26214
Nikhil p rao <nikhilprao9066@gmail.com>
2023-10-25 13:59:54 -0400

    built the gause elimination method for it

--- GE_partial_pivoting.py
+++ GE_partial_pivoting.py
@@ -17,11 +17,22 @@ print(b)
 def gaussElimination(A, b):
     n = len(b)
     x = np.zeros(n)
-    # print("Matrix A:\n", A)
-    # print("Vector b:\n", b)
+    #print("Matrix A:\n", A)
+    #print("Vector b:\n", b)
+
+    for i in range(1, n):
+        factor = A[i, i - 1] / A[i - 1, i - 1]
+        A[i] -= factor * A[i - 1]
+        # print("Vector b after elimination in row {i}:\n{a}")
+        # print(b[i])
+        b[i] -= factor * b[i - 1]
+        # print("Vector b after elimination in row {i}:\n{b}")
+        # print(b[i])
 
     x[-1] = b[-1] / A[-1, -1]
     for i in range(n - 2, -1, -1):
         x[i] = (b[i] - np.dot(A[i, i + 1:], x[i + 1:])) / A[i, i]
+
+    return xA[i, i]
 
 x = gaussElimination(A, b)
